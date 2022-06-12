a1ed45e0e14a336d0ef1710011f38fb57cc79568
birkan <b.561birkan@gmail.com>
2022-06-12 17:03:35 +0300

    equal, ac, percent and pm buttons are working now

--- calculator.js
+++ calculator.js
@@ -21,11 +21,36 @@ btnContainer.addEventListener("click", (e) => {
     chooseOperator(e.target.textContent);
     updateDisplay();
   }
+  if (e.target.classList.contains("equal")) {
+    calculate();
+    updateDisplay();
+  }
+  if (e.target.classList.contains("delete")) {
+    previousOperand = "";
+    currOperand = "";
+    operation = "";
+    updateDisplay();
+  }
+  if (e.target.classList.contains("plusMinus")) {
+    if (!currOperand) return;
+    currOperand *= -1;
+    updateDisplay();
+  }
+  if (e.target.classList.contains("percent")) {
+    if (!currOperand) return;
+    currOperand /= 100;
+    updateDisplay();
+  }
 });
 
 const appendNumber = (num) => {
   //   console.log(num);
   if (num === "0" && currOperand === "0") return; // to avoid serial zeros when currOperand = 0
+  if (currOperand === "0" && num !== ".") {
+    // if first number 0 and second number will be not "." , first 0 will be deleted
+    currOperand = num;
+    return; // this lines to avoid these situation 025 or 0356
+  }
   if (num === "." && currOperand.includes(".")) return; // to avoid serial decimals like `...`
 
   if (currOperand.length > 9) return; // to avoid overflow from div
@@ -34,12 +59,15 @@ const appendNumber = (num) => {
 };
 
 const updateDisplay = () => {
+  if (currOperand.toString.length > 10) {
+    currOperand = Number(currOperand).toExponential(5);
+  }
   currDisp.textContent = currOperand;
   prevDisp.textContent = `${previousOperand} ${operation}`;
 };
 
 const chooseOperator = (op) => {
-  //   console.log(op);
+  console.log(op);
   if (previousOperand) {
     calculate();
   }
@@ -70,8 +98,10 @@ const calculate = () => {
       calculation = prev / current;
       break;
     default:
-      break;
+      return; // it should be out of the function when operation is not matching.
   }
-  console.log(calculation);
+  //   console.log(calculation);
   currOperand = calculation;
+  previousOperand = "";
+  operation = "";
 };
